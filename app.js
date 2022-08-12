cabaf7195ab51422b0cda0eb28d0b2a360db7758
Linh Nguyen <nvvlinh2001@gmail.com>
2022-08-12 12:33:40 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express()
 
-const contactsRouter = require("./app/routes/contact.route");
+
 
 app.use(cors())
 app.use(express.json())
@@ -14,4 +15,20 @@ app.get("/", (req, res) =>{
 
 app.use("api/contacts", contactsRouter);
 
+// handle 404 response
+app.use((req, res, next) => {
+    // Code o day se chay khi khong co route duoc dinh nghia nao khop voi yeu cau. Goi next() de chuyen sang middleware xu ly loi
+    return next(new ApiError(404, "resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    // Middleware xử lý lỗi tập trung.
+    // Trong các đoạn code xử lý ở các route, gọi next(error)
+    // sẽ chuyển về middleware xử lý lỗi này
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+    });
+    
 module.exports = app;
